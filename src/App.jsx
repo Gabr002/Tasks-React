2506d0684233f874b5a36a9b56fb6f56d144fdeb
Gabr002 <gdantaslopes62@gmail.com>
2024-02-20 11:30:09 -0300

    Adicionando button remove, exebição do selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import {v4 as uuidv4} from 'uuid' 
+
 import Tasks from './components/Tasks';
 import './App.css'
 import AddTask from './components/AddTask';
@@ -18,24 +20,44 @@ const App = () => {
     }
   ]);
 
+  const handleTaskClick = (TaskId) => {
+    const newTasks = tasks.map (task => {
+      if(task.id === TaskId) return { ...task, completed: !task.completed}
+
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
   const handleTaskAddition = (taskTitle) => {
     const newTasks = [
       ...tasks, 
       {
         title: taskTitle,
-        id: Math.random(10),
+        id: uuidv4(),
         completed: false
       },
     ];
 
     setTasks(newTasks);
   };
+
+  const handleTaskDeletion = (TaskId) => {
+    const newTasks = tasks.filter(task => task.id !== TaskId)
+
+    setTasks(newTasks);
+  }
   
   return(
     <>
       <div className="container">
         <AddTask handleTaskAddition={handleTaskAddition} />
-        <Tasks tasks={tasks}/>
+        <Tasks 
+          tasks={tasks} 
+          handleTaskClick={handleTaskClick}
+          handleTaskDeletion={handleTaskDeletion}
+        />
       </div>
     </>
   );
